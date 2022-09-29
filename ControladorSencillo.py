fd4718f84dab12aad5677605a908e53d1c8c5097
anderson <adguerreroc@unal.edu.co>
2022-09-29 10:57:13 -0500

    show window for edit data with document

--- ControladorSencillo.py
+++ ControladorSencillo.py
@@ -13,7 +13,7 @@ class Coordinador():
         #recibir desde la vista el documento
         def recibirDocVista(self,d):
             return self.__mi_modelo.recibirDoc(d)
-        
+
 
 def main():
     app=QApplication([])
@@ -21,8 +21,8 @@ def main():
 	#indexamos la vista a la instancia de la clase controlador
     mi_vista=VentanaPrincipal()
     mi_controlador=Coordinador(mi_vista,mi_modelo)
-	#con el metodo del objeto mi vista de la clase Ventana Principal
-	#conectamos el controlador e igualmente a mi modelo
+        #con el metodo del objeto mi vista de la clase Ventana Principal
+        #conectamos el controlador e igualmente a mi modelo
     mi_modelo.asignarCoordinadorM(mi_controlador)
     mi_vista.asignarCoordinadorV(mi_controlador)
     mi_vista.show()
--- VistaSencilla.py
+++ VistaSencilla.py
@@ -32,40 +32,44 @@ class VentanaPrincipal(QMainWindow):
             self.__miControlador=c
         #funcion para enviar los datos recolectados al controlador
         def recibir_info2daVen(self,n,d,e):
-            r=self.__miControlador(self,n,d,e)
+            r=self.__miControlador.recibirInfoVista(n,d,e)
             QMessageBox.information(self,'mensaje informativo',r)
         #enviar documento desde ventana editar
         def enviar_documento(self,d):
             s=self.__miControlador.recibirDocVista(d)
-            QMessageBox.information(self,'mensaje informativo',s) 
+            aux='No existe datos con ese documento'
+            if s==aux:
+              QMessageBox.information(self,'mensaje informativo',s)
+            else:
+              self.abrir_ventana_ingresar()
 #ventana ingresar datos creada
 class VentanaIngresar(QDialog):
-	def __init__(self,ppal=None):
-	   super().__init__(ppal)
-	   loadUi("ventana_ingreso.ui",self)
-	   #Este atributo coloca a la ventana Principal como padre
-	   self.__ventana_padre=ppal
-	   self.setup()
+        def __init__(self,ppal=None):
+           super().__init__(ppal)
+           loadUi("ventana_ingreso.ui",self)
+           #Este atributo coloca a la ventana Principal como padre
+           self.__ventana_padre=ppal
+           self.setup()
 
-	def setup(self):
-	    self.campo_nombre.setValidator(QRegExpValidator(QRegExp("[a-zA-Z ]+")))
-	    self.campo_documento.setValidator(QIntValidator())
-	    self.campo_equipo.setValidator(QRegExpValidator(QRegExp("[a-zA-Z ]+")))
-	    self.boton_ingresar.accepted.connect(self.opcion_aceptar)
-	    self.boton_ingresar.rejected.connect(self.opcion_cancelar)
+        def setup(self):
+            self.campo_nombre.setValidator(QRegExpValidator(QRegExp("[a-zA-Z ]+")))
+            self.campo_documento.setValidator(QIntValidator())
+            self.campo_equipo.setValidator(QRegExpValidator(QRegExp("[a-zA-Z ]+")))
+            self.boton_ingresar.accepted.connect(self.opcion_aceptar)
+            self.boton_ingresar.rejected.connect(self.opcion_cancelar)
 
-	def opcion_aceptar(self):
-	    n=self.campo_nombre.text()
-	    #print(n)
-	    d=self.campo_documento.text()
-	    #print(d)
-	    e=self.campo_equipo.text()
-	    #print(e)
-	    self.__ventana_padre.recibir_info2daVen(n,d,e)
-	    self.__ventana_padre.show()
+        def opcion_aceptar(self):
+            n=self.campo_nombre.text()
+            #print(n)
+            d=self.campo_documento.text()
+            #print(d)
+            e=self.campo_equipo.text()
+            #print(e)
+            self.__ventana_padre.recibir_info2daVen(n,d,e)
+            self.__ventana_padre.show()
 
-	def opcion_cancelar(self):
-	    self.__ventana_padre.show()
+        def opcion_cancelar(self):
+            self.__ventana_padre.show()
 
 class VentanaEditar(QDialog):
     def __init__(self,ppal=None):
